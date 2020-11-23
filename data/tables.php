32197023a305eaf8f1063e4ce418da235820c78c
RishabhAgarwal-2001 <45998880+RishabhAgarwal-2001@users.noreply.github.com>
2020-11-23 12:11:52 +0530

    Foreign Key Constraints Set

--- data/tables.php
+++ data/tables.php
@@ -1,13 +1,18 @@
 <?php
+    $title = "Home Page";
+    include('../includes/head.php');
+?>
+<div class="container">
+<?php
     $user = 'root';
     $pass = '';
     $db = 'dbms_pro';
     $db_conn = new mysqli('localhost', $user, $pass, $db) or die("Unable To Connect");
     
-    $db_conn->query("DROP TABLE users");
-    $db_conn->query("DROP TABLE train");
-    $db_conn->query("DROP TABLE ticket");
     $db_conn->query("DROP TABLE passenger");
+    $db_conn->query("DROP TABLE ticket");
+    $db_conn->query("DROP TABLE train");
+    $db_conn->query("DROP TABLE users");
     $db_conn->query("DROP TABLE ac_coach");
     $db_conn->query("DROP TABLE sleeper_coach");
 
@@ -24,10 +29,16 @@
     )";
 
     if($db_conn->query($user_table)){
-        echo "Created User Table<br><hr>";
+        echo '<div class="alert alert-success" role="alert">
+            User Table Created!!!
+        </div>';
+        // echo "Created User Table<br><hr>";
     }
     else{
-        echo "Error While Creating User Table<br><hr>";
+        echo '<div class="alert alert-danger" role="alert">
+            User Table Creation Failed!!!
+        </div>';
+        // echo "Error While Creating User Table<br><hr>";
     }
 
     // Train Information
@@ -40,45 +51,66 @@
     )";
 
     if($db_conn->query($train_table)){
-        echo "Created Train Table<br><hr>";
+        echo '<div class="alert alert-success" role="alert">
+            Train Table Created!!!
+        </div>';
+        // echo "Created Train Table<br><hr>";
     }
     else{
-        echo "Error While Creating Train Table<br><hr>";
+        echo '<div class="alert alert-danger" role="alert">
+            Train Table Creation Failed!!!
+        </div>';
+        // echo "Error While Creating Train Table<br><hr>";
     }
 
     $ticket_table = "CREATE TABLE ticket(
         pnr INT NOT NULL AUTO_INCREMENT,
-        booked_by INT NOT NULL REFERENCES users(id),
-        train_number VARCHAR(5) NOT NULL REFERENCES train(train),
-        date DATE NOT NULL REFERENCES train(date),
+        booked_by INT NOT NULL,
+        train_number VARCHAR(5) NOT NULL,
+        date DATE NOT NULL,
         coach_type CHAR(1) NOT NULL,
         num_passengers INT NOT NULL,
-        PRIMARY KEY(pnr)
+        PRIMARY KEY(pnr),
+        FOREIGN KEY (booked_by) REFERENCES users(id),
+        FOREIGN KEY (train_number, date) REFERENCES train(train, date)
     )";
 
     if($db_conn->query($ticket_table)){
-        echo "Created Ticket Table<br><hr>";
+        echo '<div class="alert alert-success" role="alert">
+            Ticket Table Created!!!
+        </div>';
+        // echo "Created Ticket Table<br><hr>";
     }
     else{
-        echo "Error While Creating Ticket Table<br><hr>";
+        echo '<div class="alert alert-danger" role="alert">
+            Ticket Table Creation Failed!!!
+        </div>';
+        // echo "Error While Creating Ticket Table<br><hr>";
     }
 
     $passenger_table = "CREATE TABLE passenger(
-        pnr VARCHAR(10) NOT NULL REFERENCES ticket(pnr),
+        pnr INT NOT NULL,
         coach_number VARCHAR(10),
         seat_number INT,
         p_name VARCHAR(128) NOT NULL,
         p_age INT NOT NULL,
         p_gender CHAR(1) NOT NULL,
         seat_type CHAR(2) NOT NULL,
-        PRIMARY KEY(pnr, coach_number, seat_number)
+        PRIMARY KEY(pnr, coach_number, seat_number),
+        FOREIGN KEY (pnr) REFERENCES ticket(pnr)
     )";
 
     if($db_conn->query($passenger_table)){
-        echo "Created Passenger Table<br><hr>";
+        echo '<div class="alert alert-success" role="alert">
+            Passenger Table Created!!!
+        </div>';
+        // echo "Created Passenger Table<br><hr>";
     }
     else{
-        echo "Error While Creating Passenger Table<br><hr>";
+        echo '<div class="alert alert-danger" role="alert">
+            Passenger Table Creation Failed!!!
+        </div>';
+        // echo "Error While Creating Passenger Table<br><hr>";
     }
 
     $sleeper_coach_table = "CREATE TABLE sleeper_coach(
@@ -87,10 +119,16 @@
     )";
 
     if($db_conn->query($sleeper_coach_table)){
-        echo "Created Sleeper Coach Table<br><hr>";
+        echo '<div class="alert alert-success" role="alert">
+            Sleeper Coach Table Created!!!
+        </div>';
+        // echo "Created Sleeper Coach Table<br><hr>";
     }
     else{
-        echo "Error While Creating Sleeper Coach Table<br><hr>";
+        echo '<div class="alert alert-danger" role="alert">
+            Sleeper Coach Table Creation Failed!!!
+        </div>';
+        // echo "Error While Creating Sleeper Coach Table<br><hr>";
     }
 
     $ac_coach_table = "CREATE TABLE ac_coach(
@@ -99,10 +137,24 @@
     )";
 
     if($db_conn->query($ac_coach_table)){
-        echo "Created AC Coach Table<br><hr>";
+        echo '<div class="alert alert-success" role="alert">
+            AC Coach Table Created!!!
+        </div>';
+        // echo "Created AC Coach Table<br><hr>";
     }
     else{
-        echo "Error While Creating AC Coach Table<br><hr>";
+        echo '<div class="alert alert-danger" role="alert">
+            AC Coach Table Creation Failed!!!
+        </div>';
+        // echo "Error While Creating AC Coach Table<br><hr>";
+    }
+
+    $admin_user = 'CALL createAdmin("admin", "admin@gmail.com", "1234567890", "root")';
+    if($db_conn->query($admin_user)){
+        echo '<div class="alert alert-success" role="alert">
+            Admin User: e-mail => admin@gmail.com, password => root
+        </div>';
     }
 
 ?>
+</div>
